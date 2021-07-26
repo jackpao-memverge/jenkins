c80d0e482f63b0f1b85a14235859d7f52cc0c0c2
jack.pao@memverge.com <jack.pao@memverge.com>
2021-07-26 14:20:40 -0700

    add test group

--- pipeline_snapshot_suite.groovy
+++ pipeline_snapshot_suite.groovy
@@ -20,13 +20,16 @@ curl -X POST -H 'Content-type: application/json' --data \
 BUILD_LOCATION: %s \n \
 JOBS LIST PASSED: \n%s \n \
 JOBS LIST FAILED (NIGHTLY NANNY PLEASE TRIAGE): \n%s"}' \
-%s
+%sß
 '''
+
+MVTEST_SUITE = "${env.JOB_BASE_NAME}"
 
 node ("cicd_vm") {
     currentBuild.displayName = "#${BUILD_NUMBER} ${BUILD_LABEL}"
     currentBuild.result = 'SUCCESS'
    try{
+       MVTEST_GROUP = "[Nightly] Snapshot test suite, basic"
        stage ("Nightly Snapshot test suite")
         {
             b1_result = 'SUCCESSFUL'
@@ -42,6 +45,7 @@ node ("cicd_vm") {
         currentBuild.result = 'UNSTABLE'
     }
    try{
+       MVTEST_GROUP = "[Nightly] Snapshot test suite, Hugepage cache 5G"
        stage ("Nightly Snapshot test suite, Hugepage cache 5G")
         {
             b1_result = 'SUCCESSFUL'
@@ -57,6 +61,7 @@ node ("cicd_vm") {
         currentBuild.result = 'UNSTABLE'
     }
    try{
+       MVTEST_GROUP = "[Nightly] Snapshot test suite, Hugepage cache 5G"
        stage ("Nightly Snapshot test suite, regular cache 5G")
         {
             b1_result = 'SUCCESSFUL'
@@ -72,6 +77,7 @@ node ("cicd_vm") {
         currentBuild.result = 'UNSTABLE'
     }
     try{
+       MVTEST_GROUP = "[Nightly] Snapshot test suite, Hugepage cache 5G"
        stage ("Nightly Snapshot test suite, mvsnapd service")
         {
             b1_result = 'SUCCESSFUL'
@@ -87,10 +93,11 @@ node ("cicd_vm") {
         currentBuild.result = 'UNSTABLE'
     }
     try{
-       stage ("Nightly Snapshot test suite, mm bin installation skip for now")
+       MVTEST_GROUP = "[Nightly] Snapshot test mm bin installation suite"
+       stage ("Nightly Snapshot test suite, mm bin installation")
         {
             b1_result = 'SUCCESSFUL'
-            //snapshot_suite_all_mm_bin(snapshot_tests)
+            snapshot_suite_all_mm_bin(snapshot_tests)
             if(b1_result == 'FAILURE') {
                 echo "Stage failed"
                 sh "echo Stage failed;exit 1"
@@ -126,20 +133,17 @@ def snapshot_suite_all(list) {
                db_per_server = 10
                //node_worker = "jack_worker"
            }
-           b1 = build job: "pipeline_mvmalloc_nightly_test", propagate: false, parameters: [string(name: "BUILD_LABEL", value: "Nightly regression ${list[i]}"),
-           string(name: "HOSTS_DAX_MAP", value: "${HOSTS_DAX_MAP}"),
-           //string(name: "HOSTS_DAX_MAP", value: "padre-d.eng.memverge.com=/dev/dax0.0,/dev/dax1.0"),
-           string(name: "MV_TESTS", value: "${list[i]}"),
-           string(name: "USER_PW", value: "memverge"), string(name: "NUMBER_SERVER", value: "${num_server}"),
-           //string(name: "USER_PW", value: "memverge"), string(name: "NUMBER_SERVER", value: "5"),
-           string(name: "DB_PER_SERVER", value: "${db_per_server}"), string(name: "RECORD_PER_DB", value: "1000000"),
-           string(name: "BUILD_LOCATION", value: "${BUILD_DIR}/${RHEL_VER}/${BUILD_DATE}/${pkg_name}"),
-           //string(name: "BUILD_LOCATION", value: "${BUILD_DIR}/rhel8/${BUILD_DATE}/mvmm*x86_64.tgz"),
-           string(name: "SNAPSHOT_DEPTH", value: "5"), booleanParam(name: "SKIP_NUMA_CTL", value:"${skip_numa}"),
-           string(name: "SNAPSHOT_WIDTH", value: "5"), string(name: "KX_HOME", value: "${kx_home}"),
-           string(name: "MVTEST_BRANCH", value: "${MVTEST_BRANCH}"),
-           booleanParam(name: "TCMS_DRY_RUN", value: false), booleanParam(name: "TCMS_TRACE", value: false),
-           //[$class: "LabelParameterValue", name: "node", label: "${node_worker}"]
+           b1 = build job: "pipeline_mvmalloc_nightly_test", propagate: false, parameters: [
+                string(name: "BUILD_LABEL", value: "${MVTEST_SUITE} | ${MVTEST_GROUP} | ${list[i]}"),
+                string(name: "HOSTS_DAX_MAP", value: "${HOSTS_DAX_MAP}"),
+                string(name: "MV_TESTS", value: "${list[i]}"),
+                string(name: "USER_PW", value: "memverge"), string(name: "NUMBER_SERVER", value: "${num_server}"),
+                string(name: "DB_PER_SERVER", value: "${db_per_server}"), string(name: "RECORD_PER_DB", value: "1000000"),
+                string(name: "BUILD_LOCATION", value: "${BUILD_DIR}/${RHEL_VER}/${BUILD_DATE}/${pkg_name}"),
+                string(name: "SNAPSHOT_DEPTH", value: "5"), booleanParam(name: "SKIP_NUMA_CTL", value:"${skip_numa}"),
+                string(name: "SNAPSHOT_WIDTH", value: "5"), string(name: "KX_HOME", value: "${kx_home}"),
+                string(name: "MVTEST_BRANCH", value: "${MVTEST_BRANCH}"),
+                booleanParam(name: "TCMS_DRY_RUN", value: false), booleanParam(name: "TCMS_TRACE", value: false)
            ]
            try{
                if(b1.result == 'FAILURE'|| b1.result == 'ABORTED') {
@@ -175,22 +179,19 @@ def snapshot_suite_all_huge(list) {
                db_per_server = 10
                node_worker = "jack_worker"
            }           
-           b1 = build job: "pipeline_mvmalloc_nightly_test", propagate: false, parameters: [string(name: "BUILD_LABEL", value: "Nightly regression ${list[i]}"),
-           string(name: "HOSTS_DAX_MAP", value: "${HOSTS_DAX_MAP}"),
-           //string(name: "HOSTS_DAX_MAP", value: "padre-d.eng.memverge.com=/dev/dax0.0,/dev/dax1.0"),
-           string(name: "MV_TESTS", value: "${list[i]}"),
-           string(name: "USER_PW", value: "memverge"), string(name: "NUMBER_SERVER", value: "${num_server}"),
-           //string(name: "USER_PW", value: "memverge"), string(name: "NUMBER_SERVER", value: "5"),
-           string(name: "DB_PER_SERVER", value: "${db_per_server}"), string(name: "RECORD_PER_DB", value: "1000000"),
-           string(name: "BUILD_LOCATION", value: "${BUILD_DIR}/${RHEL_VER}/${BUILD_DATE}/${pkg_name}"),
-           //string(name: "BUILD_LOCATION", value: "${BUILD_DIR}/rhel8/${BUILD_DATE}/mvmm*x86_64.tgz"),
-           string(name: "SNAPSHOT_DEPTH", value: "5"), booleanParam(name: "SKIP_NUMA_CTL", value:"${skip_numa}"),
-           string(name: "SNAPSHOT_WIDTH", value: "5"), string(name: "KX_HOME", value: "${kx_home}"),
-           string(name: "MVTEST_BRANCH", value: "${MVTEST_BRANCH}"),
-           booleanParam(name: "TCMS_DRY_RUN", value: false), booleanParam(name: "TCMS_TRACE", value: false),
-           booleanParam(name: 'HugePageDram', value: true), booleanParam(name: 'RegularDram', value: false), string(name: 'DramCacheGB', value: '5'),
-           booleanParam(name: 'start_mvmallocd_service', value: true),
-           //[$class: "LabelParameterValue", name: "node", label: "${node_worker}"]
+           b1 = build job: "pipeline_mvmalloc_nightly_test", propagate: false, parameters: [
+                string(name: "BUILD_LABEL", value: "${MVTEST_SUITE} | ${MVTEST_GROUP} | ${list[i]}"),
+                string(name: "HOSTS_DAX_MAP", value: "${HOSTS_DAX_MAP}"),
+                string(name: "MV_TESTS", value: "${list[i]}"),
+                string(name: "USER_PW", value: "memverge"), string(name: "NUMBER_SERVER", value: "${num_server}"),
+                string(name: "DB_PER_SERVER", value: "${db_per_server}"), string(name: "RECORD_PER_DB", value: "1000000"),
+                string(name: "BUILD_LOCATION", value: "${BUILD_DIR}/${RHEL_VER}/${BUILD_DATE}/${pkg_name}"),
+                string(name: "SNAPSHOT_DEPTH", value: "5"), booleanParam(name: "SKIP_NUMA_CTL", value:"${skip_numa}"),
+                string(name: "SNAPSHOT_WIDTH", value: "5"), string(name: "KX_HOME", value: "${kx_home}"),
+                string(name: "MVTEST_BRANCH", value: "${MVTEST_BRANCH}"),
+                booleanParam(name: "TCMS_DRY_RUN", value: false), booleanParam(name: "TCMS_TRACE", value: false),
+                booleanParam(name: 'HugePageDram', value: true), booleanParam(name: 'RegularDram', value: false), string(name: 'DramCacheGB', value: '5'),
+                booleanParam(name: 'start_mvmallocd_service', value: true)
            ]
            try{
                 if(b1.result == 'FAILURE'|| b1.result == 'ABORTED') {
@@ -226,21 +227,18 @@ def snapshot_suite_all_regular(list) {
                db_per_server = 10
                node_worker = "jack_worker"
            }           
-           b1 = build job: "pipeline_mvmalloc_nightly_test", propagate: false, parameters: [string(name: "BUILD_LABEL", value: "Nightly regression ${list[i]}"),
-           string(name: "HOSTS_DAX_MAP", value: "${HOSTS_DAX_MAP}"),
-           //string(name: "HOSTS_DAX_MAP", value: "padre-d.eng.memverge.com=/dev/dax0.0,/dev/dax1.0"),
-           string(name: "MV_TESTS", value: "${list[i]}"),
-           string(name: "USER_PW", value: "memverge"), string(name: "NUMBER_SERVER", value: "${num_server}"),
-           //string(name: "USER_PW", value: "memverge"), string(name: "NUMBER_SERVER", value: "5"),
-           string(name: "DB_PER_SERVER", value: "${db_per_server}"), string(name: "RECORD_PER_DB", value: "1000000"),
-           string(name: "BUILD_LOCATION", value: "${BUILD_DIR}/${RHEL_VER}/${BUILD_DATE}/${pkg_name}"),
-           //string(name: "BUILD_LOCATION", value: "${BUILD_DIR}/rhel8/${BUILD_DATE}/mvmm*x86_64.tgz"),
-           string(name: "SNAPSHOT_DEPTH", value: "5"), booleanParam(name: "SKIP_NUMA_CTL", value: "${skip_numa}"),
-           string(name: "SNAPSHOT_WIDTH", value: "5"), string(name: "KX_HOME", value: "${kx_home}"),
-           string(name: "MVTEST_BRANCH", value: "${MVTEST_BRANCH}"),
-           booleanParam(name: "TCMS_DRY_RUN", value: false), booleanParam(name: "TCMS_TRACE", value: false),
-           booleanParam(name: 'HugePageDram', value: false), booleanParam(name: 'RegularDram', value: true), string(name: 'DramCacheGB', value: '5'),
-           //[$class: "LabelParameterValue", name: "node", label: "${node_worker}"]
+           b1 = build job: "pipeline_mvmalloc_nightly_test", propagate: false, parameters: [
+                string(name: "BUILD_LABEL", value: "${MVTEST_SUITE} | ${MVTEST_GROUP} | ${list[i]}"),
+                string(name: "HOSTS_DAX_MAP", value: "${HOSTS_DAX_MAP}"),
+                string(name: "MV_TESTS", value: "${list[i]}"),
+                string(name: "USER_PW", value: "memverge"), string(name: "NUMBER_SERVER", value: "${num_server}"),
+                string(name: "DB_PER_SERVER", value: "${db_per_server}"), string(name: "RECORD_PER_DB", value: "1000000"),
+                string(name: "BUILD_LOCATION", value: "${BUILD_DIR}/${RHEL_VER}/${BUILD_DATE}/${pkg_name}"),
+                string(name: "SNAPSHOT_DEPTH", value: "5"), booleanParam(name: "SKIP_NUMA_CTL", value: "${skip_numa}"),
+                string(name: "SNAPSHOT_WIDTH", value: "5"), string(name: "KX_HOME", value: "${kx_home}"),
+                string(name: "MVTEST_BRANCH", value: "${MVTEST_BRANCH}"),
+                booleanParam(name: "TCMS_DRY_RUN", value: false), booleanParam(name: "TCMS_TRACE", value: false),
+                booleanParam(name: 'HugePageDram', value: false), booleanParam(name: 'RegularDram', value: true), string(name: 'DramCacheGB', value: '5')
            ]
            try{
                 if(b1.result == 'FAILURE'|| b1.result == 'ABORTED') {
@@ -276,22 +274,19 @@ def snapshot_suite_all_mvsnapd(list) {
                db_per_server = 10
                node_worker = "jack_worker"
            }
-           b1 = build job: "pipeline_mvmalloc_nightly_test", propagate: false, parameters: [string(name: "BUILD_LABEL", value: "Nightly regression mvsnapd ${list[i]}"),
-           string(name: "HOSTS_DAX_MAP", value: "${HOSTS_DAX_MAP}"),
-           //string(name: "HOSTS_DAX_MAP", value: "padre-d.eng.memverge.com=/dev/dax0.0,/dev/dax1.0"),
-           string(name: "MV_TESTS", value: "${list[i]}"),
-           string(name: "USER_PW", value: "memverge"), string(name: "NUMBER_SERVER", value: "${num_server}"),
-           //string(name: "USER_PW", value: "memverge"), string(name: "NUMBER_SERVER", value: "5"),
-           string(name: "DB_PER_SERVER", value: "${db_per_server}"), string(name: "RECORD_PER_DB", value: "1000000"),
-           string(name: "BUILD_LOCATION", value: "${BUILD_DIR}/${RHEL_VER}/${BUILD_DATE}/${pkg_name}"),
-           //string(name: "BUILD_LOCATION", value: "${BUILD_DIR}/rhel8/${BUILD_DATE}/mvmm*x86_64.tgz"),
-           string(name: "SNAPSHOT_DEPTH", value: "5"), booleanParam(name: "SKIP_NUMA_CTL", value:"${skip_numa}"),
-           string(name: "SNAPSHOT_WIDTH", value: "5"), string(name: "KX_HOME", value: "${kx_home}"),
-           string(name: "MVTEST_BRANCH", value: "${MVTEST_BRANCH}"),
-           booleanParam(name: "TCMS_DRY_RUN", value: false), booleanParam(name: "TCMS_TRACE", value: false),
-           booleanParam(name: 'start_mvmallocd_service', value: true),
-           booleanParam(name: 'mvsnapd_service', value: true),
-           //[$class: "LabelParameterValue", name: "node", label: "${node_worker}"]
+           b1 = build job: "pipeline_mvmalloc_nightly_test", propagate: false, parameters: [
+                string(name: "BUILD_LABEL", value: "${MVTEST_SUITE} | ${MVTEST_GROUP} | ${list[i]}"),
+                string(name: "HOSTS_DAX_MAP", value: "${HOSTS_DAX_MAP}"),
+                string(name: "MV_TESTS", value: "${list[i]}"),
+                string(name: "USER_PW", value: "memverge"), string(name: "NUMBER_SERVER", value: "${num_server}"),
+                string(name: "DB_PER_SERVER", value: "${db_per_server}"), string(name: "RECORD_PER_DB", value: "1000000"),
+                string(name: "BUILD_LOCATION", value: "${BUILD_DIR}/${RHEL_VER}/${BUILD_DATE}/${pkg_name}"),
+                string(name: "SNAPSHOT_DEPTH", value: "5"), booleanParam(name: "SKIP_NUMA_CTL", value:"${skip_numa}"),
+                string(name: "SNAPSHOT_WIDTH", value: "5"), string(name: "KX_HOME", value: "${kx_home}"),
+                string(name: "MVTEST_BRANCH", value: "${MVTEST_BRANCH}"),
+                booleanParam(name: "TCMS_DRY_RUN", value: false), booleanParam(name: "TCMS_TRACE", value: false),
+                booleanParam(name: 'start_mvmallocd_service', value: true),
+                booleanParam(name: 'mvsnapd_service', value: true)
            ]
            try{
                if(b1.result == 'FAILURE'|| b1.result == 'ABORTED') {
@@ -325,21 +320,23 @@ def snapshot_suite_all_mm_bin(list) {
                num_server = 5
                db_per_server = 10
            }           
-           b1 = build job: "pipeline_mvmalloc_nightly_test", propagate: false, parameters: [string(name: "BUILD_LABEL", value: "Nightly regression ${list[i]}"),
-           string(name: "HOSTS_DAX_MAP", value:  "${HOSTS_DAX_MAP}"),
-           //string(name: "HOSTS_DAX_MAP", value: "padre-d.eng.memverge.com=/dev/dax0.0,/dev/dax1.0"),
-           string(name: "MV_TESTS", value: "${list[i]}"),
-           string(name: "USER_PW", value: "memverge"), string(name: "NUMBER_SERVER", value: "${num_server}"),
-           //string(name: "USER_PW", value: "memverge"), string(name: "NUMBER_SERVER", value: "5"),
-           string(name: "DB_PER_SERVER", value: "${db_per_server}"), string(name: "RECORD_PER_DB", value: "1000000"),
-           string(name: "BUILD_LOCATION", value: "${BUILD_DIR}/${RHEL_VER}/${BUILD_DATE}/${pkg_name}"),
-           //string(name: "BUILD_LOCATION", value: "${BUILD_DIR}/rhel8/${BUILD_DATE}/mvmm*x86_64.tgz"),
-           string(name: "SNAPSHOT_DEPTH", value: "5"), booleanParam(name: "SKIP_NUMA_CTL", value: true),
-           string(name: "SNAPSHOT_WIDTH", value: "5"), string(name: "KX_HOME", value: "${kx_home}"),
-           string(name: "MVTEST_BRANCH", value: "${MVTEST_BRANCH}"),
-           string(name: "MM_INSTALL_BIN", value: "${mm_install_path}"),
-           booleanParam(name: "TCMS_DRY_RUN", value: false), booleanParam(name: "TCMS_TRACE", value: false),
-           booleanParam(name: 'HugePageDram', value: false), booleanParam(name: 'RegularDram', value: true), string(name: 'DramCacheGB', value: '5')]
+           b1 = build job: "pipeline_mvmalloc_nightly_test", propagate: false, parameters: [
+                string(name: "BUILD_LABEL", value: "${MVTEST_SUITE} | ${MVTEST_GROUP} | ${list[i]}"),
+                string(name: "HOSTS_DAX_MAP", value:  "${HOSTS_DAX_MAP}"),
+                //string(name: "HOSTS_DAX_MAP", value: "padre-d.eng.memverge.com=/dev/dax0.0,/dev/dax1.0"),
+                string(name: "MV_TESTS", value: "${list[i]}"),
+                string(name: "USER_PW", value: "memverge"), string(name: "NUMBER_SERVER", value: "${num_server}"),
+                //string(name: "USER_PW", value: "memverge"), string(name: "NUMBER_SERVER", value: "5"),
+                string(name: "DB_PER_SERVER", value: "${db_per_server}"), string(name: "RECORD_PER_DB", value: "1000000"),
+                string(name: "BUILD_LOCATION", value: "${BUILD_DIR}/${RHEL_VER}/${BUILD_DATE}/${pkg_name}"),
+                //string(name: "BUILD_LOCATION", value: "${BUILD_DIR}/rhel8/${BUILD_DATE}/mvmm*x86_64.tgz"),
+                string(name: "SNAPSHOT_DEPTH", value: "5"), booleanParam(name: "SKIP_NUMA_CTL", value: true),
+                string(name: "SNAPSHOT_WIDTH", value: "5"), string(name: "KX_HOME", value: "${kx_home}"),
+                string(name: "MVTEST_BRANCH", value: "${MVTEST_BRANCH}"),
+                string(name: "MM_INSTALL_BIN", value: "${mm_install_path}"),
+                booleanParam(name: "TCMS_DRY_RUN", value: false), booleanParam(name: "TCMS_TRACE", value: false),
+                booleanParam(name: 'HugePageDram', value: false), booleanParam(name: 'RegularDram', value: true), string(name: 'DramCacheGB', value: '5')
+            ]
            try{
                 if(b1.result == 'FAILURE'|| b1.result == 'ABORTED') {
                 //if(b1.result == 'FAILURE') {
